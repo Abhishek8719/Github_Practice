4fc6d4dd060f81fc3e75dbe903e4a54343fc34fb
Abhishek8719 <cabhishekchoudhary31@gmail.com>
2025-07-05 14:51:36 +0530

    Added new functions

--- util.jsx
+++ util.jsx
@@ -3,8 +3,16 @@ function Hell0 (a,b){
 }
 console.log(Hell0(1,2));
 
-// function World (a,b){
-//     return (a*b)
-// }
+function World (a,b){
+    return (a*b)
+}
 
-// console.log(World(3,4));
+console.log(World(3,4));
+const util = {
+    add: function(a, b) {
+        return a + b;
+    },
+    multiply: function(a, b) {
+        return a * b;
+    }
+};
